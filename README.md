e1f4e014748b59d69fdb0ddc0ffbaa405ab887e6
dev-hanz-ops <97662291+dev-hanz-ops@users.noreply.github.com>
2022-07-25 12:28:38 +0200

    install gh cli

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Install GH CLI action
+
+Selfhosted runners do not come with the GH CLI out of the box. This action is an easy-to-use way to install it.
+
+## Usage
+
+```yaml
+- name: Install GH CLI
+  uses: dev-hanz-ops/install-gh-cli-action
+  with:
+    gh-cli-version: 2.14.2 # optional, see action.yml for current default
+```
---
+++ src/run.js
@@ -0,0 +1,42 @@
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as path from 'path';
+
+async function run() {
+  try {
+    let version = core.getInput('gh-cli-version');
+    if (version) {
+      await getGhCli(version);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function getGhCli(version) {
+  let toolPath = tc.find('gh-cli', version);
+
+  if (!toolPath) {
+    toolPath = await downloadGhCli(version);
+  }
+
+  toolPath = path.join(toolPath, 'bin');
+  core.addPath(toolPath);
+}
+
+async function downloadGhCli(version) {
+  const toolDirectoryName = `gh-cli-v${version}`;
+  const downloadUrl = `https://github.com/cli/cli/releases/download/v${version}/gh_${version}_linux_amd64.tar.gz`;
+  console.log(`downloading ${downloadUrl}`);
+
+  try {
+    const downloadPath = await tc.downloadTool(downloadUrl);
+    const extractedPath = await tc.extractTar(downloadPath);
+    let toolRoot = path.join(extractedPath, toolDirectoryName);
+    return await tc.cacheDir(toolRoot, 'gh-cli', version);
+  } catch (err) {
+    throw err;
+  }
+}
+
+run();
